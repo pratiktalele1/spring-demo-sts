f7206c7845e1a381d377daf89cf66858a69fc3a9
pratiktalele1 <pratiktalele1@gmail.com>
2021-09-16 12:17:40 +0530

    Welcome to spring-demo-sts-app

---
+++ src/main/java/com/bridgelabz/app/SpringDemoStsAppApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoStsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoStsAppApplication.class, args);
+		
+	}
+
+}
